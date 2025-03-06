fb9f41614e3a3a7300893507dd807d703bf0d4a7
Rakesh799 <rakeshchoudhury074@gmail.com>
2025-03-06 15:39:17 +0530

    added localstorage logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useState, useRef } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
-
   const [inputval, setInputval] = useState("")
-  const ref = useRef()
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem("todos"))
+    if (storedTodos) {
+      setTodos(storedTodos);
+    }
+  }, [])
+  
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
 
   const handleClick = () => {
-    let ul = document.querySelector(".alltodos")
-    ul.innerHTML += `<li class="bg-[#550036] text-white py-1 px-2 rounded-lg w-[250px] font-bold mb-2">${inputval}</li>`
-    ref.current.value = "";
+    if (inputval.trim().length > 0) {
+      setTodos((prev) => [...prev, inputval])
+    }
+    setInputval("")
   }
-  
 
   return (
     <>
@@ -18,9 +28,8 @@ function App() {
         <div className="bg-[#550036] text-center text-white font-bold text-3xl p-2">Manage all Your Todos at One Place</div>
 
         <div className='flex md:flex-row flex-col justify-center items-center gap-2 mt-8'>
-          <label className='text-[#550036] font-bold' htmlFor="">Enter a todo: </label>
+          <label className='text-[#550036] font-bold' htmlFor="inputval">Enter a todo: </label>
           <input
-            ref={ref}
             onChange={(e) => setInputval(e.target.value)}
             value={inputval}
             type="text"
@@ -32,7 +41,13 @@ function App() {
         <div className='flex flex-col justify-center items-center gap-2 w-full'>
           <h1 className='font-bold text-2xl underline text-[#550036] mt-4'>Your Todos:</h1>
           <ul className='alltodos'>
-            
+            {todos.map((todo, index) => (
+              <li
+                key={index}
+                className='bg-[#550036] text-white py-1 px-2 rounded-lg w-[240px] mb-2'>
+                {todo}
+              </li>
+            ))}
           </ul>
         </div>
 
