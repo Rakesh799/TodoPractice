f6f247bfb8216298742d114ac63f4bf30b386cc8
Rakesh799 <rakeshchoudhury074@gmail.com>
2025-03-14 14:07:37 +0530

    added update and completed functionality

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
   const handleClick = () => {
     if (inputval.trim().length > 0) {
-      setTodos((prev) => [...prev, inputval])
+      setTodos((prev) => [...prev, { text: inputval, completed: false }])
     }
     setInputval("")
   }
@@ -25,6 +25,19 @@ function App() {
   const deleteTodo = (indexToRemove) => {
     setTodos((todos) => todos.filter((todo, index) => index !== indexToRemove))
   }
+
+  const editTodo = (index) => {
+    setInputval(todos[index].text)
+    setTodos((prevTodos) => prevTodos.filter((todo, i) => i !== index))
+  }
+
+  const markAsDone = (index) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo, i) =>
+        i === index ? { ...todo, completed: !todo.completed } : todo
+      ))
+  }
+
 
   return (
     <>
@@ -46,20 +59,45 @@ function App() {
           <h1 className='font-bold text-2xl underline text-[#550036] mt-4'>Your Todos:</h1>
           <div className='alltodos'>
             {todos.map((todo, index) => (
-              <div key={index} className="flex justify-between items-center bg-[#550036] text-white py-1 px-2 rounded-lg w-[240px] mb-2">
+              <div key={index} className="flex justify-between items-center bg-[#550036] text-white py-1 px-2 rounded-lg w-[300px] md:w-[500px] mb-2">
 
-                <div className='max-w-48 overflow-hidden'>{todo}</div>
-                <button onClick={() => deleteTodo(index)} className='cursor-pointer'>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="white"
-                  >
-                    <path d="M6.225 4.811l5.775 5.774 5.774-5.774 1.414 1.414-5.774 5.775 5.774 5.774-1.414 1.414-5.774-5.774-5.775 5.774-1.414-1.414 5.775-5.774-5.775-5.775z" />
-                  </svg>
-                </button>
+                <div className="flex gap-2 items-center">
+                  <div>
+                    <input
+                      type="checkbox"
+                      checked={todo.completed}
+                      onChange={() => markAsDone(index)}
+                      className='w-4 h-4 cursor-pointer accent-[#550036] mt-1'
+                    />
+                  </div>
+
+                  <div className={`max-w-56 md:max-w-[450px] overflow-hidden ${todo.completed ? "line-through" : ""}`}>{todo.text}</div>
+                </div>
+
+                <div className="flex gap-2">
+                  <button onClick={() => editTodo(index)} className='cursor-pointer'>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      fill="white"
+                    >
+                      <path d="M3 21v-3.75L16.81 3.44a1.5 1.5 0 0 1 2.12 0l1.63 1.63a1.5 1.5 0 0 1 0 2.12L6.75 21H3zm16.85-15.43l-1.63-1.63L16.1 6.06l1.63 1.63 2.12-2.12zM5 19h2.25L17.06 9.19 14.94 7.06 5 16.94V19z" />
+                    </svg>
+                  </button>
+                  <button onClick={() => deleteTodo(index)} className='cursor-pointer'>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      fill="white"
+                    >
+                      <path d="M6.225 4.811l5.775 5.774 5.774-5.774 1.414 1.414-5.774 5.775 5.774 5.774-1.414 1.414-5.774-5.774-5.775 5.774-1.414-1.414 5.775-5.774-5.775-5.775z" />
+                    </svg>
+                  </button>
+                </div>
 
               </div>
 
